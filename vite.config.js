484c87decef5ca9cb7caf29abbc245ab12ce3e83
arguspermono <arguspermono19@gmail.com>
2026-06-12 17:53:45 +0700

    change base url

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/web_frontend_jejaknusa/',
+  base: '/frontend_web_jejaknusa/',
   build: {
     rollupOptions: {
       input: {
